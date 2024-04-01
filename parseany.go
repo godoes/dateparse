82197ca5010b19f61c1fb4d92441c8915f4d605d
liutianqi <zixizixi@vip.qq.com>
2024-04-01 17:06:13 +0800

    New date format 1st November 2020
    Ref: araddon/dateparse/pull/128

--- parseany.go
+++ parseany.go
@@ -398,6 +398,12 @@ iterRunes:
 				p.stateDate = dateDigitChineseYear
 			case ',':
 				return nil, unknownErr(datestr)
+			case 's', 'S', 'r', 'R', 't', 'T', 'n', 'N':
+				// 1st January 2018
+				// 2nd Jan 2018 23:59
+				// st, rd, nd, st
+				p.stateDate = dateAlphaWsMonthSuffix
+				i--
 			default:
 				continue
 			}
